0db565bdf05c79ab98099d8c405fb17fd9b7e613
nelisa <ntl.elisa@gmail.com>
2022-11-18 13:00:12 +0100

    js

--- cours.js
+++ cours.js
@@ -1,57 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const vignette=document.getElementsByClassName('cours-disponibles-vignettes')
+    let compteur= 0;
+    const nombre=document.getElementsByClassName('compteur');
 
-//HyperText Markup Language
-// un langage de balisage (avec des balises) conçu pour représenter des pages web / que comprend les navigateurs. Donc ce sont des instructions pour les navigateurs. 
-//cf schéma page 6 : le navigateur va interprêter le fichier HTML, tout comme le CSS pour réaliser l'affichage d'un site sur votre écran.
-//https://developer.mozilla.org/fr/docs/Learn/CSS/First_steps/How_CSS_works
-
- 
-
-//HTML 
-// décomposition d'une balise 
-// https://developer.mozilla.org/fr/docs/Glossary/HTML#concept_et_syntaxe
-
-
-//listes 
-// des éléménts (des balises)
-
-// des attributs 
-// https://developer.mozilla.org/fr/docs/Web/HTML/Attributes
+    Array.from(vignette).forEach(cours => {
+        cours.addEventListener('click', () => {
+            cours.classList.toggle("selected");
+            compteur = document.getElementsByClassName('selected').length;
+            console.log(compteur);
+            nombre.innerHTML=compteur;
+        });
+    });
+    
 
 
-// installation d'Emmet (raccourci avec le !)
-// montrer Doctype 
-//https://developer.mozilla.org/fr/docs/Glossary/Doctype
-// html : indiquer au navigateur qu'il lit un document html, c'est le premier élément de l'arbre du DOM qu'il recréé
-// head : 	L'élément HTML <head> fournit des informations générales (métadonnées) sur le document, incluant son titre et des liens ou des définitions vers des scripts et feuilles de style.
-// meta : 
-// link : L'élément HTML <link> définit la relation entre le document courant et une ressource externe. Cet élément peut être utilisé pour définir un lien vers une feuille de style, vers les icônes utilisées en barre de titre ou comme icône d'application sur les appareils mobiles.
+    // function comptage() {
+    // compteur++;
+    // document.getElementById("nombreClics").textContent   = nombreClics;
+    // }
 
+    // document.getElementById("boutonClic").addEventListener("click", comptage);
 
-//qu'est ce que vous connaissez comme principale balise ?
-// cf. slide 13 
-// faire les exercices d'Rbean sans la sentinelle (à partir de red_square)
-// faire tous les exercices de la journée 2 (sauf bordered table et spaced table)
-// on ne fait pas les exercices de la journée 3
-
-// faire la démo inline-block et flex
-// il y a des éléments qui prennent toute la largeur par défaut : ce sont des éléments blocks
-//les div, les sections, les p, les h etc.
-// il y a des éléments inline par défaut : les span, strong, a
-// inline-block : c'est pour mettre des éléments block en inline
-//https://openclassrooms.com/fr/courses/1603881-creez-votre-site-web-avec-html5-et-css3/3298561-faites-votre-mise-en-page-avec-flexbox
-
-
-//CSS 
-//la classe est prioritaire
-// l'id est plus fort de la classe
-// plus j'ai de détail plus c'est fort div .class 
-//l'id aura toujours la priorité
-// quand on doit surcoucher (sans toucher à des id par exemple), important peut sauver la mise 
-
-// on utilise pas les px pour les widths
-// quand on veut centrer : pas de px 
-// si on veut quelque chose d'immobile en px (ex. sur un slider), pour la position absolute par exemple
-// les rem (unités de mesure pour le responsive) 
-
-
-// utilisation de tailwind et saas dans les projets pour plus tard 
+    // document.getElementById("desactiver").addEventListener("click", function(e) {
+    //     document.getElementById("boutonClic").removeEventListener("click", comptage);
+    // });
+});
